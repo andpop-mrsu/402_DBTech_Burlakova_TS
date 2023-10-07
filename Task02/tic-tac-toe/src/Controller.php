3b84dcbeff4f414b230b6c884f4235e8436129ae
btat7 <tania.bourlakova2002@yandex.ru>
2023-10-07 23:54:04 +0300

    Task02

---
+++ Task02/tic-tac-toe/src/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace btat7\TicTacToe\Controller;
+
+use btat7\TicTacToe\View\View;
+use function cli\line;
+
+class Controller
+{
+    public function startGame()
+    {
+        line("Start the game!");
+
+        $view = new View();
+        $view->showGame();
+    }
+}
---
+++ Task02/tic-tac-toe/src/View.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace btat7\TicTacToe\View;
+
+use function cli\line;
+
+class View
+{
+    public function showGame()
+    {
+
+        line("This is the game intereface");
+    }
+}
